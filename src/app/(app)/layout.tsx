f966eb88b58cd43a3933bb4e0f8c3e8eadaa6afc
cristof <casasleon.j@gmail.com>
2026-05-05 00:13:49 -0500

    fix: revert BaseCajaModal dynamic import (ssr:false not allowed in SC)

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,10 +1,8 @@
 import { Providers } from '@/components/providers'
 import { Toaster } from 'sonner'
-import dynamic from 'next/dynamic'
+import BaseCajaModal from '@/components/base-caja-modal'
 import { UpdateNotification } from '@/components/update-notification'
 import { AppShell } from './app-shell'
-
-const BaseCajaModal = dynamic(() => import('@/components/base-caja-modal'), { ssr: false })
 
 export default async function AppLayout({ children }: { children: React.ReactNode }) {
   return (
