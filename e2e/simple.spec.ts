12e2a921b129b0634a9d9ce8bf546d7c9460cdad
cristof <casasleon.j@gmail.com>
2026-05-25 00:15:29 -0500

    chore: remove debug and temp test files

--- e2e/simple.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('simple test', async ({ page }) => {
-  await page.goto('http://localhost:3000/login')
-  await expect(page).toHaveTitle(/Agua Bambú/)
-})
